3947f24d31ce46dc4c1f8b293763ef9392d69971
Randall Rushing <randall.rushing@gmail.com>
2015-01-09 21:20:05 -0800

    shamu: introduce a much less crippled liblights
    Each color (RGB) of the tri-color LED sits on it's own GPIO, which
    is a rather unique design. As such, the leds-qpnp driver is not
    currently designed for robust LED controls in this configuration.
    Instead we set default values to absolute red, green or blue:
    notification color is green, low battery is red, medium battery is
    blue, and battery full is green. Notification LED takes precedence
    over battery LED, however when battery is charging at full capacity
    both will be green.
    
    This hal introduces basic user controls including custom charge and
    notification colors. Blink and breathe are not supported at this time.
    Through settings controls the user can set any color combination they
    want, although due to limitations colors other than absolute rgb may
    show two-toned visual artifacts.
    
    Change-Id: Ica9bc2aa820d1f7182e56aea54a66f7d1f9cde79

--- aicp.mk
+++ aicp.mk
@@ -11,6 +11,8 @@ $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
+DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aicp_shamu
 PRODUCT_BRAND := google
