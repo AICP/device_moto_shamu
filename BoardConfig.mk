7ce7a199e388c4ed336fc5cd919eee7fe05a76d2
Nick Kralevich <nnk@google.com>
2015-02-06 14:35:06 -0800

    Allow init to rm /dev/diag
    Commit 456d9d6c4b50f39fdd176bafd2ae007c047f352c ensures that
    /dev/diag is always removed on boot on user builds. Allow for it
    in SELinux policy.
    
    Change-Id: Ib4748705b9aeb089ed461888da8d57f37bc0d62a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,7 @@ BOARD_SEPOLICY_UNION += \
         domain.te \
         file.te \
         gsiffd.te \
+        init.te \
         irsc_util.te \
         mdm_helper.te \
         mediaserver.te \
