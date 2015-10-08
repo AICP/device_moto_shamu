81b1e668d8eca25f2e4d17c144e58d5c609f4830
doc HD <doc.divxm@gmail.com>
2015-10-08 17:13:06 +0300

    shamu: AICPfy
    Change-Id: I448493899cf6540daf1aa13e8e90b292696d44c0

--- aicp.mk
+++ aicp.mk
@@ -2,17 +2,17 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_shamu
+PRODUCT_NAME := aicp_shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 
