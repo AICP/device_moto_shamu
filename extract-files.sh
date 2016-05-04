3a6fae2b9bd7c7c4142f2d516ccf74ac03fb9ac6
nuclearmistake <nuclearmistake@gmail.com>
2016-05-04 08:33:16 +0300

    extract-files: extract shamu blobs directly from .tgz factory images
    Adopted for shamu from: http://review.cyanogenmod.org/#/c/131312/2
    
    Squash of commits by Lance R. Vick (lance@lancervick.net)
    With fixes by Eric McCann / nuclearmistake (nuclearmistake@gmail.com)
    
    TODO:
    - make extracting system and vendor image contents not require sudo?
    - make this work on darwin (I doubt it does)
    - ...?
    
    implemented procedure:
    - Downloads the factory image archive (if missing)
       - checks the md5sum
    - Unpacks system.img and vendor.img
       - Converts system and vendor simgs to imgs
       - Mounts imgs of system and vendor
           - (this presently entails "sudo")
       - Copies contents out of mountpoint
       - umounts
    - foreach file in proprietary_files:
       - copy to appropriate location in vendor/huawei
       - fixes xml file headers
       - oat2dexes apks and jars
    - setup-makefiles.sh does so
    
    Squashed commit of the following:
    
    commit f2d69386131e12983b13b1390fac8f0bc559c5bb
    Author: nuclearmistake <nuclearmistake@gmail.com>
    Date:   Mon Feb 1 15:54:08 2016 -0500
    
        Update extractor for MMB29Q
    
        Change-Id: I058c3f319c81aa5c3d7dec727948ed42cb9adde9
    
    commit 95214be0b12403ff162c6f1db20065348b997a7f
    Author: nuclearmistake <nuclearmistake@gmail.com>
    Date:   Mon Jan 4 18:51:34 2016 -0500
    
        Update angler extractor for MMB29P image URL
    
        Change-Id: I0b41054963debcbdcb50248fc4ca00b803d2d160
    
    commit 261c29b6226621f9d5e30376120795ac2d01976e
    Author: nuclearmistake <nuclearmistake@gmail.com>
    Date:   Mon Dec 7 22:12:43 2015 -0500
    
        angler: update extractor for mmb29m blobs
    
        Change-Id: I3f056abb72bbc133371425b28f6aa38d80469883
    
    commit 4a8a58d47bdeeaabf6b751fa9b4457550eb2f69a
    Author: nuclearmistake <nuclearmistake@gmail.com>
    Date:   Mon Dec 28 13:31:52 2015 -0500
    
        setup-makefiles: centralize signature conditionalization
    
        Change-Id: Ie3bc24b2522c0cb7e09fbd0710a73eb51283f85c
    
    commit 25081f33d08455e9316fe264b4591f336c1c372f
    Author: nuclearmistake <nuclearmistake@gmail.com>
    Date:   Wed Dec 30 10:33:09 2015 -0500
    
        ims: copy 32bit libs too
    
        Change-Id: I4bc7110a8303e348bb51c2feec9fcaa70869532b
    
    commit 6bbd3d601c79fe278e0af0057784965eee4ba643
    Author: nuclearmistake <nuclearmistake@gmail.com>
    Date:   Tue Dec 15 11:50:30 2015 -0500
    
        extract-files: fixup deprecate oat2dex conditions
    
        Change-Id: Ia67b9fd5dad3fbdaf1c7246b285bf4edefcfc561
    
    commit 8e36b1123af2a8fa895a262d00548752f6f86ce0
    Author: nuclearmistake <nuclearmistake@gmail.com>
    Date:   Tue Dec 1 08:53:56 2015 -0500
    
        Fix derped absolute outdir
    
        Change-Id: I8116fada055fb99fd7e7f26cb4038e6adeadfa24
    
    commit 30ec5ac52730184c562cee5fc392d96fbddab876
    Author: Lance R. Vick <lance@lrvick.net>
    Date:   Wed Dec 2 00:32:24 2015 -0800
    
        use /tmp as .tmp confuses brunch
    
    commit 3f381ffaa1b5f5f38cd6d1bcf612b5e43b7e84a9
    Author: Lance R. Vick <lance@lrvick.net>
    Date:   Wed Dec 2 00:27:30 2015 -0800
    
        fixed derps from not testing again after var cleanup
    
    commit d8b2d37b1ef760557723081300c206d68ba85b7a
    Author: Lance R. Vick <lance@lrvick.net>
    Date:   Mon Nov 30 22:34:29 2015 -0800
    
        extract files directly from google hosted factory image
    
    Change-Id: Id6cfcd09db3330ce18c041af8590cbcdf10145ad

--- extract-files.sh
+++ extract-files.sh
@@ -1,62 +1,172 @@
-#!/bin/bash
+#!/bin/bash -e
 
-export VENDOR=motorola
-export DEVICE_VENDOR=moto
-export DEVICE=shamu
+vendor=motorola
+device=shamu
+build=mob30i
 
-# Check to see if the user passed a folder in to extract from rather than adb pull
-if [ $# -eq 1 ]; then
-    COPY_FROM=$1
-    test ! -d "$COPY_FROM" && echo error reading dir "$COPY_FROM" && exit 1
-fi
+factory_url=https://dl.google.com/dl/android/aosp/shamu-mob30i-factory-21357b09.tgz
+factory_md5=21357b098c59c7eb654d4904012ce36f38fce79b
 
-set -e
 
-function extract() {
-    for FILE in `egrep -v '(^#|^$)' $1`; do
-        echo "Extracting /system/$FILE ..."
-        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-        DEST=${PARSING_ARRAY[1]}
-        if [ -z $DEST ]; then
-            DEST=$FILE
-        fi
-        DIR=`dirname $FILE`
-        if [ ! -d $2/$DIR ]; then
-            mkdir -p $2/$DIR
-        fi
-        if [ "$COPY_FROM" = "" ]; then
-            # Try destination target first
-            if [ -f /system/$DEST ]; then
-                adb pull /system/$DEST $2/$DEST
-            else
-                # if file does not exist try OEM target
-                if [ "$?" != "0" ]; then
-                    adb pull /system/$FILE $2/$DEST
-                fi
-            fi
-        else
-            # Try destination target first
-            if [ -f $COPY_FROM/$DEST ]; then
-                cp $COPY_FROM/$DEST $2/$DEST
-            else
-                # if file does not exist try OEM target
-                if [ "$?" != "0" ]; then
-                    DIR=`dirname $DEST`
-                    if [ ! -d $2/$DIR ]; then
-                        mkdir -p $2/$DIR
-                    fi
-                    cp $COPY_FROM/$FILE $2/$DEST
-                fi
-            fi
-        fi
-    done
+self_dir="$(dirname $(readlink -f $0))"
+proprietary_files=$self_dir/proprietary-blobs.txt
+device_base=$ANDROID_BUILD_TOP/vendor/$vendor/$device/proprietary
+
+function md5verify(){
+  local md5=$1
+  local file=$2
+  md5sum --quiet -c <( echo "$md5 $file" )
+  if [ $? -ne 0 ]; then
+    echo "$file failed MD5 hash check"
+    return 1
+  fi
 }
 
-DEVICE_BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $DEVICE_BASE/*
+function fetch(){
+  local url=$1
+  local md5=$2
+  local cachedir=$ANDROID_BUILD_TOP/.fetchcache # need a better location
+  local outfile=$cachedir/${url##*/}
 
-# Extract the device specific files
-extract ../../$DEVICE_VENDOR/$DEVICE/device-proprietary-files.txt $DEVICE_BASE
+  mkdir -p $cachedir
 
-./setup-makefiles.sh
+  if [ ! -f $outfile ]; then
+    wget "$url" -O "$outfile"
+  fi
+  if md5verify "$md5" "$outfile"; then
+    echo $outfile
+  else
+    rm $outfile
+    fetch $url $md5
+  fi
+}
+
+function checkutils() {
+  local utils="$@"
+  for util in $utils; do
+	  echo -n " * Checking for $util..."
+	  which $util 2>&1 > /dev/null
+	  if [ $? -eq 0 ]; then
+		printf " ok!\n";
+	  else
+		printf " not found!\n";
+		exit 1;
+	  fi
+  done
+}
+
+function build_smali(){
+  local outdir="$1"
+  rm -rf $outdir
+  git clone https://github.com/JesusFreke/smali $outdir
+  cd $outdir
+  ./gradlew build
+  cd -
+}
+
+function oat2dex(){
+  local file="$1"
+  local tmpdir="$2"
+
+  local smali_jar=$tmpdir/smali/smali/build/libs/smali.jar
+  local baksmali_jar=$tmpdir/smali/baksmali/build/libs/baksmali.jar
+
+  if [ ! -f "$smali_jar" ]; then
+    build_smali "$tmpdir/smali"
+  fi
+
+  if [ ! -e "`dirname $file`/oat/arm/`basename $file ."${file##*.}"`.odex" ] && [ ! -e "`dirname $file`/oat/arm64/`basename $file ."${file##*.}"`.odex" ]; then
+    return 0
+  fi
+
+  mkdir -p $tmpdir/dexout
+  for x in "`dirname $file`/oat/arm/`basename $file ."${file##*.}"`.odex" "`dirname $file`/oat/arm64/`basename $file ."${file##*.}"`.odex"; do
+    [ ! -e "$x" ] && continue
+    java -jar "$baksmali_jar" -x -o "$tmpdir/dexout" -c boot.oat -d "$tmpdir/system/framework/arm64" -d "$mpdir/system/framework/arm" "$x"
+    ls -l $tmpdir/dexout | wc -l
+  done
+  java -jar "$smali_jar" "$tmpdir/dexout" -o "$tmpdir/classes.dex"
+  rm -rf "$tmpdir/dexout"
+}
+
+function unpack_factory(){
+  local factory_url=$1
+  local factory_md5=$2
+  local outdir=$3
+  local factory_file=$(fetch "$factory_url" "$factory_md5")
+  local device=$(basename $factory_file | sed 's/\(^\w\+\).*/\1/g')
+  local build=$(basename $factory_file | sed 's/^\w\+-\([a-z0-9]\+\)-.*/\1/g')
+
+  mkdir -p $outdir/extracted
+
+  tar -C "$outdir" -xvzf "$factory_file"
+
+  unzip  -d $outdir -o $outdir/$device-$build/image-$device-${build}.zip
+
+  for image in system vendor; do
+    simg2img $outdir/${image}.img $outdir/${image}.ext4.img
+	mkdir -p $outdir/${image}_mount
+
+	# I really hate using sudo here but I see no other choice
+	# Any other ideas to extract these are welcome
+	sudo mount -o loop $outdir/${image}.ext4.img $outdir/${image}_mount
+	sudo cp -r $outdir/${image}_mount $outdir/$image
+    sudo umount $outdir/${image}_mount
+    sudo rm -rf $outdir/${image}_mount
+    sudo chown -R $USER $outdir/${image}
+  done
+
+}
+
+function extract(){
+  local factory_url=$1
+  local factory_md5=$2
+  local proprietary_files=$3
+  local outbase=$4
+  local tmpdir="/tmp/extract-files/$device/$build"
+
+  mkdir -p $4
+  mkdir -p $tmpdir
+
+  unpack_factory "$factory_url" "$factory_md5" "$tmpdir"
+
+  for file in $(egrep -v '(^#|^$)' $proprietary_files); do
+      echo "Extracting $file ..."
+      oldifs=$IFS IFS=":" parsing_array=($file) IFS=$oldifs
+      srcfile=$(echo ${parsing_array[0]} | sed -e "s/^-//g")
+      destfile=${parsing_array[1]}
+      if [ -z "$destfile" ]; then
+          destfile="$srcfile"
+      fi
+      destfile=$(echo "$destfile" | sed 's|^system/||')
+      destdir=$(dirname "$destfile")
+      if [ ! -d "$outbase/$destdir" ]; then
+          mkdir -p "$outbase/$destdir"
+      fi
+
+      cp "$tmpdir/$srcfile" "$outbase/$destfile"
+
+      # Fixup xml files
+      if [[ "$outbase/$destfile" =~ .xml$ ]]; then
+          xmlheader=$(grep '^<?xml version' "$outbase/$destfile")
+          grep -v '^<?xml version' "$outbase/$destfile" > "$outbase/$destfile".temp
+          (echo "$xmlheader"; cat "$outbase/$destfile".temp ) > "$outbase/$destfile"
+          rm "$outbase/$destfile".temp
+      fi
+      if [[ "$destfile" =~ .(apk|jar)$ ]]; then
+          oat2dex "$tmpdir/$srcfile" $tmpdir
+          if [ -e "$tmpdir/classes.dex" ]; then
+              zip -gjq "$outbase/$destfile" "$tmpdir/classes.dex"
+              rm "$tmpdir/classes.dex"
+              echo "Updated $outbase/$destfile from odex files"
+          fi
+      fi
+  done
+
+}
+
+checkutils simg2img unzip md5sum sudo wget
+
+extract "$factory_url" "$factory_md5" "$proprietary_files" "$device_base"
+
+bash $self_dir/setup-makefiles.sh
