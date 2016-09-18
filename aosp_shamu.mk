ded57ab04e93a41aa314d145666d30d844454fec
Pranav Vashi <neobuddy89@gmail.com>
2016-09-18 13:17:16 +0300

    shamu: Updates from  neobuddy89/android_device_moto_shamu
    Change-Id: Iabfee9f3c6029e5b13e7d12ce471acc97bc0deac

--- aosp_shamu.mk
+++ aosp_shamu.mk
@@ -25,16 +25,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_NAME := aosp_shamu
 PRODUCT_DEVICE := shamu
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Shamu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
-#PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, device/moto/shamu/device.mk)
 $(call inherit-product-if-exists, vendor/motorola/shamu/shamu-vendor.mk)
 
-PRODUCT_NAME := aosp_shamu
-
 PRODUCT_PACKAGES += \
     Launcher3
-
