ac8e1e57d33f6641a74271974599ec9c4d99af2e
doc HD <doc.divxm@gmail.com>
2016-10-04 01:27:38 +0300

    shamu: AICPfy extract-files script
    Change-Id: I07937ceb3205360d15b7ce51a1961dfc7e1115de

--- extract-files.sh
+++ extract-files.sh
@@ -23,9 +23,9 @@ DEVICE=shamu
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+AICP_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$CM_ROOT"/vendor/aicp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -49,7 +49,7 @@ else
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$AICP_ROOT"
 
 extract "$MY_DIR"/device-proprietary-files.txt "$SRC"
 
