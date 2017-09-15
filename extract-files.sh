f0464b2810a6a7475e8f79b2070ec479b5de3dc9
Miccia <bono.michele94@gmail.com>
2017-09-15 11:40:47 +0200

    shamu: aicpfy
    Change-Id: Ifb3b0686d378508efcf132b612e38931ed9fa744

--- extract-files.sh
+++ extract-files.sh
@@ -24,9 +24,9 @@ DEVICE=shamu
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+AICP_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$AICP_ROOT"/vendor/aicp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -50,7 +50,7 @@ else
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$AICP_ROOT"
 
 extract "$MY_DIR"/device-proprietary-files.txt "$SRC"
 
